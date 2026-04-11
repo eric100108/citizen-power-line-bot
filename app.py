43962dbcc45327b8c9439a70d4b11f062247c5ec
邱浚維 <weijun705@gmail.com>
2026-04-11 10:50:31 +0800

    add simple web page

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import sqlite3
 import requests
 from flask import Flask, jsonify, request
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -117,6 +118,14 @@ def reply_line_message(reply_token, text):
     print("LINE reply status:", response.status_code)
     print("LINE reply body:", response.text)
 
+@app.route("/menu")
+def menu():
+    return"""
+    <h1>公民電廠服務</h1>
+    <p><a href="/faq">查詢常見問題</a></p>
+    <p><a href="/calc?amount=10000">投資試算</a></p>
+    <p><a href="/progress">案廠進度</a></p>
+    """
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
